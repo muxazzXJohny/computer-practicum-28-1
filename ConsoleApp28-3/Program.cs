98aeac7165db3d4e06edf990ef5258156dc573b6
muxazzXJohny <maracoco334@gmail.com>
2025-03-10 17:31:15 +0200

    Add files via upload

---
+++ ConsoleApp28-3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = { 1, 3, 6, 7, 10, 12, 9, 24 };
+        }
+        public static void Elements(int[] omas)
+        {
+            int maxelement = omas[0];
+            int maxindex = 0;
+            int a = 0;
+            for (int i = 0; i < omas.Length; i++)
+            {
+                if (maxelement < omas[i])
+                {
+                    maxindex = i;
+                }
+            }
+            a = omas[0];
+            omas[0] = omas[maxindex];
+            omas[maxindex] = a;
+        }
+    }
+}
+    
+
