738970097ba37fc1ff28b98a146ca966c97b0c96
muxazzXJohny <maracoco334@gmail.com>
2025-03-10 17:16:49 +0200

    Add files via upload

---
+++ ConsoleApp28-1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = new int[14];
+            VvodMass(omas);
+            Elements(omas);
+            Console.ReadKey();
+        }
+        public static void VvodMass(int[] omas)
+        {
+            for (int i = 0; i < omas.Length; i++)
+            {
+                Console.Write($"omas[{i}]=");
+                omas[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        public static void Elements(int[] omas)
+        {
+            int n = 0;
+            foreach (int element in omas)
+            {
+                if ((element & 1) == 0)
+                {
+                    n++;
+                }
+            }
+            Console.WriteLine($"Количество чётных елементов={n}");
+        }
+    }
+}
+
