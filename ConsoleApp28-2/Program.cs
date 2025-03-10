2e65cd7b34b3c4ac5db0c3b85b433128070297b7
muxazzXJohny <maracoco334@gmail.com>
2025-03-10 17:18:33 +0200

    Add files via upload

---
+++ ConsoleApp28-2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = new int[9];
+            RndOMass(omas);
+            Elements(omas);
+            Console.ReadLine();
+        }
+        public static void RndOMass(int[] omas)
+        {
+            Random random = new Random();
+            for (int i = 0; i < omas.Length; i++)
+            {
+                omas[i] = random.Next(-25, 25);
+                Console.WriteLine($"omas[{i}]={omas[i]}");
+            }
+        }
+        public static void Elements(int[] omas)
+        {
+            int count = 0;
+            int count1 = 0;
+            for (int i = 0; i < omas.Length; i++)
+            {
+                if ((omas[i] & 1) == 0)
+                {
+                    count++;
+                }
+                if ((omas[i] & 2) == 0)
+                {
+                    count1++;
+                }
+            }
+            Console.WriteLine(count);
+            Console.WriteLine(count1);
+        }
+    }
+}
---
+++ ConsoleApp28-2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp28-2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp28-2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1473d895-ac55-4490-9429-a10ce66fb366")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
